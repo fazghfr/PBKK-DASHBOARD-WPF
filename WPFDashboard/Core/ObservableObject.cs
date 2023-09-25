3e156fb8ce860de454328c55b88095059a85dde5
fauzan <fauzanalghifari963@gmail.com>
2023-09-25 19:51:20 +0700

    Add project files.

---
+++ WPFDashboard/Core/ObservableObject.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+namespace WPFDashboard.Core
+{
+    class ObservableObject
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ WPFDashboard/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WPFDashboard.Core;
+using WPFDashboard.MVVM.View;
+
+namespace WPFDashboard.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public HomeViewModel HomeVM { get; set; }
+        public MoreViewModel MoreVM { get; set; }
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand MoreViewCommand { get; set; }
+
+
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            MoreVM = new MoreViewModel();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            MoreViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = MoreVM;
+            });
+        }
+    }
+}
